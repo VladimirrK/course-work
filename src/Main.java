e88a57156c95b4555444477de3063917027656a5
VladimirrK <AngerPrideLust@gmail.com>
2023-01-21 03:42:14 +0500

    first commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        listOfEmployees();
     }
-}
+
+    public static void listOfEmployees() {
+
+        Employee[] employee = new Employee[5];
+        employee[0] = new Employee("Василий Иванович", 1, 54600, Employee.counter);
+        employee[1] = new Employee("Игорь Петрович", 3, 61200, Employee.counter);
+        employee[2] = new Employee("Евгений Дмитриевич", 2, 48300, Employee.counter);
+        employee[3] = new Employee("Пётр Петрович", 4, 36900, Employee.counter);
+        employee[4] = new Employee("Василий Геннадьевич", 5, 74100, Employee.counter);
+
+        informationForAllEmployees(employee);
+        printSumSalary(employee);
+        printMinSalary(employee);
+    }
+
+    public static void informationForAllEmployees(Employee[] employee) {
+        for (Employee i : employee) {
+            System.out.println(i);
+        }
+    }
+
+    public static void printSumSalary(Employee[] employee) {
+        int sum = 0;
+        for (int i = 0; i < employee.length; i++) {
+            sum += employee[i].getSalary();
+        }
+        System.out.println("Сумма затрат на зарплаты в месяц составила - " + sum);
+    }
+
+    public static void printMinSalary(Employee[] employee) {
+        for (int i = 0; i < employee.length; i++) {
+            if (i < employee[i].getSalary()) {
+                i = employee[i].getSalary();
+            }
+            System.out.println("Сотрудник с минимальной зарплатой - " + employee[i].getSalary());
+        }
+    }
+}w
